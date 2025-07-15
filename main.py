a38154fa083bad182b66d30f0739c57568834b5d
Dennis Lappee <dennis.lappee@outlook.com>
2025-07-15 22:38:30 +0200

    implemented moveable panels plus visualisation

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ from spacecraft_body import *
 
 
 if __name__ == "__main__":
-    satellite = Satellite(1, .3, .3, .8, .3,
-                          np.array([30, 30, 30, 30]), None)
+    satellite = Satellite(1.2, .4, .4, 1, .4,
+                          np.array([10, 10, 40, 40]), None)
     satellite.print_nodes()
+    satellite.visualise()
     #TODO: Include angles satellite panels in body creation (requires different position vector)
     #TODO: Think up some way to graphically represent the spacecraft body to visually confirm geometry
     #TODO: Start work on particle-panel interaction
+    #TODO: Implement some function to calculate panel moments of inertia
--- spacecraft_body.py
+++ spacecraft_body.py
@@ -1,5 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+def set_axes_equal(ax):
+    """
+    Set 3D plot axes to have equal scale so that spheres appear as spheres,
+    cubes as cubes, etc. This makes one unit along x, y or z look the same.
+    """
+    limits = np.array([
+        ax.get_xlim3d(),
+        ax.get_ylim3d(),
+        ax.get_zlim3d(),
+    ])
+    center = np.mean(limits, axis=1)
+    radius = 0.5 * np.max(limits[:, 1] - limits[:, 0])
+    ax.set_xlim3d([center[0] - radius, center[0] + radius])
+    ax.set_ylim3d([center[1] - radius, center[1] + radius])
+    ax.set_zlim3d([center[2] - radius, center[2] + radius])
+
+
 
 class Panel:
     def __init__(self, length: float, width: float, normal_vector: np.array, forward_vector: np.array, body: bool = True):
@@ -8,16 +28,18 @@ class Panel:
              [0, 1, 0],
              [0, 0, 1]]
         )
+        self.body = body
         self.length = length
         self.width = width
         self.n = normal_vector
         self.f = forward_vector
+        self.global_nodes = np.array
         ### Panel origin in centre of panel ###
         self.local_nodes = np.array((
-            [-self.length/2, self.width/2,  0],
-            [-self.length/2, -self.width/2, 0],
-            [ self.length/2, self.width/2,  0],
-            [ self.length/2, -self.width/2, 0],
+            [-self.length/2, self.width/2,  0], # rear left
+            [-self.length/2, -self.width/2, 0], # rear right
+            [ self.length/2, -self.width/2, 0], # front right
+            [ self.length/2, self.width/2,  0], # front left
         ))
         n_cross_f = np.cross(self.n, self.f)
         B = np.matrix(np.vstack((self.f, n_cross_f, self.n))).T
@@ -25,10 +47,16 @@ class Panel:
         self.initial_normal_vector = np.array([0, 0, 1])
         new_normal_vector = B @ self.initial_normal_vector
         new_forward_vector = B @ self.initial_forward_vector
-        self.local_nodes = (B @ self.local_nodes.T).T
+        if body:  # Rotate about centre of panel (NOTE: potential flaw if body panels not perpendicular)
+            self.local_nodes = (B @ self.local_nodes.T).T
+        if not body:  # Rotate around hinge point
+            self.local_nodes = self.local_nodes + np.array([-self.length/2, 0, 0]).T
+            self.local_nodes = (B @ self.local_nodes.T).T
 
     def define_global_nodes(self, position_vector: np.array):
         self.global_nodes = np.array(self.local_nodes + position_vector)
+        return
+
 
 class Satellite:
     def __init__(self, x_len: float, y_width: float, z_width: float, panel_length: float, panel_width: float,
@@ -38,37 +66,52 @@ class Satellite:
         self.z_width = z_width
         self.panel_length = panel_length
         self.panel_width = panel_width
+        self.panel_angles = panel_angles.astype(float)
         for idx, panel_angle in enumerate(panel_angles):
             while panel_angle < 0 or panel_angle > 90:
                 ValueError("Panel angle must be between 0 and 90 degrees, but is " + str(panel_angle) + " degrees.")
                 panel_angle = np.deg2rad(input("Provide a new panel angle in degrees:"))
             else:
-                self.panel_angles = np.deg2rad(panel_angles)
+                self.panel_angles[idx] = np.deg2rad(panel_angle)
         self.com = com
         self.inertia = inertia
-        self.panel_hinges = [
-            np.array([-x_len, y_width / 2, 0]),
-            np.array([-x_len, 0, z_width / 2]),
-            np.array([-x_len, -y_width / 2, 0]),
-            np.array([-x_len, 0, -z_width / 2]),
-        ]
-        self.body_panels = []
         ### COORDINATE SYSTEM STARTS AT TIP OF SPACECRAFT ###
         self.body_panel_centres = [
-            np.array([0, 0, 0]), np.array([-self.x_len, 0, 0]),
-            np.array([-self.x_len/2, self.y_width/2, 0]), np.array([-self.x_len/2, -self.y_width/2, 0]),
-            np.array([-self.x_len/2, 0, self.z_width/2]), np.array([-self.x_len/2, 0, -self.z_width/2]),
+            np.array([0, 0, 0]), np.array([-self.x_len, 0, 0]),  # front, rear
+            np.array([-self.x_len/2, self.y_width/2, 0]), np.array([-self.x_len/2, -self.y_width/2, 0]), # left, right
+            np.array([-self.x_len/2, 0, self.z_width/2]), np.array([-self.x_len/2, 0, -self.z_width/2]), # top, bottom
         ]
         self.body_forward_vectors = [
-            np.array([0, 0, 1]), np.array([0, 0, 1]),
-            np.array([1, 0, 0]), np.array([1, 0, 0]),
-            np.array([1, 0, 0]), np.array([1, 0, 0]),
+            np.array([0, 0, 1]), np.array([0, 0, 1]), # point up
+            np.array([1, 0, 0]), np.array([1, 0, 0]), # point forward
+            np.array([1, 0, 0]), np.array([1, 0, 0]), # point forward
         ]
         self.body_normal_vectors = [
-            np.array([1, 0, 0]), np.array([-1, 0, 0]),  # x forward, x backward
-            np.array([0, 1, 0]), np.array([0, -1, 0]),  # y right, y left
-            np.array([0, 0, 1]), np.array([0, 0, -1]),  # z down, z up
+            np.array([1, 0, 0]), np.array([-1, 0,  0]),  # point forward, backward
+            np.array([0, 1, 0]), np.array([0,  -1, 0]),  # point left, right
+            np.array([0, 0, 1]), np.array([0,  0,  -1]),  # point up, down
         ]
+        ### Moveable panel vectors, with ability to determine angle per panel ###
+        #TODO: There is likely room to make this code more succinct
+        self.panel_hinges = [
+            np.array([-x_len, 0, z_width / 2]),  # up
+            np.array([-x_len, 0, -z_width / 2]),  # down
+            np.array([-x_len, y_width / 2, 0]),  # left
+            np.array([-x_len, -y_width / 2, 0]),  # right
+        ]
+        self.rear_forward_vectors = [
+            np.array([np.cos(self.panel_angles[0]), 0, -np.sin(self.panel_angles[0])]), # upper rear panel
+            np.array([np.cos(self.panel_angles[1]), 0, np.sin(self.panel_angles[1])]),  # lower rear panel
+            np.array([np.cos(self.panel_angles[2]), -np.sin(self.panel_angles[2]), 0]), # left rear panel
+            np.array([np.cos(self.panel_angles[3]), np.sin(self.panel_angles[3]), 0]),  # right rear panels
+        ]
+        self.rear_normal_vectors = [
+            np.array([np.sin(self.panel_angles[0]), 0, np.cos(self.panel_angles[0])]), # upper rear panel
+            np.array([np.sin(self.panel_angles[1]), 0, -np.cos(self.panel_angles[1])]),  # lower rear panel
+            np.array([np.sin(self.panel_angles[2]), np.cos(self.panel_angles[2]), 0]), # left rear panel
+            np.array([np.sin(self.panel_angles[3]), -np.cos(self.panel_angles[3]), 0]),  # right rear panels
+        ]
+        self.panels = []
         self.panel_nodes = []
         for idx, panel_center in enumerate(self.body_panel_centres):
             if idx < 2:  # front and back
@@ -78,9 +121,14 @@ class Satellite:
             else:  # top and bottom
                 panel = Panel(self.x_len, self.z_width, self.body_normal_vectors[idx], self.body_forward_vectors[idx])
             panel.define_global_nodes(panel_center)
+            self.panels.append(panel)
             self.panel_nodes.append(panel.global_nodes)
-        # print(self.panel_nodes)
-
+        for idx, hinge_location in enumerate(self.panel_hinges):
+            panel = Panel(self.panel_length, self.panel_width, self.rear_normal_vectors[idx],
+                          self.rear_forward_vectors[idx], body=False)
+            panel.define_global_nodes(hinge_location)
+            self.panels.append(panel)
+            self.panel_nodes.append(panel.global_nodes)
 
     def print_nodes(self):
         for nodes in self.panel_nodes:
@@ -94,9 +142,43 @@ class Satellite:
         self.com = np.array([0, 0, 0])
         return
 
-        # if com == np.array([-.5, 0, 0]):
-        #     new_com()
 
-    # def visualise(self):
+    def visualise(self, show_vectors: bool = True):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+
+        for panel in self.panel_nodes:
+            verts = [panel]
+            panel_collection = Poly3DCollection(
+                verts, facecolors='skyblue', edgecolors='k',
+                linewidths=1, alpha=0.8
+            )
+            ax.add_collection3d(panel_collection)
+            ax.scatter(panel[:, 0], panel[:, 1], panel[:, 2], color='r')
+
+        if show_vectors:
+            for panel, nodes in zip(self.panels, self.panel_nodes):
+                center = nodes.mean(axis=0)
+                n_vec = panel.n / np.linalg.norm(panel.n) * 0.3
+                f_vec = panel.f / np.linalg.norm(panel.f) * 0.3
+                ax.quiver(center[0], center[1], center[2],
+                          n_vec[0], n_vec[1], n_vec[2],
+                          color='b', arrow_length_ratio=0.2, linewidth=2)
+                ax.quiver(center[0], center[1], center[2],
+                          f_vec[0], f_vec[1], f_vec[2],
+                          color='g', arrow_length_ratio=0.2, linewidth=2)
+
+            legend_elements = [ Line2D([0], [0], color='b', lw=2, label='Normal vector'),
+                                Line2D([0], [0], color='g', lw=2, label='Forward vector')]
+            ax.legend(handles=legend_elements, loc="best")
+
+        ax.set_xlabel('Length (x)')
+        ax.set_ylabel('Width (y)')
+        ax.set_zlabel('Height (z)')
+        plt.title("Satellite configuration")
+        ax.set_box_aspect([1, 1, 1])
+        set_axes_equal(ax)
+        plt.show()
+
 
 
