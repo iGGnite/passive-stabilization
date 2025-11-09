960967e299c1c366dc22d78e44f3c2ea31217d51
Dennis Lappee <dennis.lappee@outlook.com>
2025-11-09 16:16:27 +0100

    Added readme and updated gitignore

--- main.py
+++ main.py
@@ -16,11 +16,15 @@ if __name__ == "__main__":
     if run:
         sim = PassiveStabilization()
         sim.create_cubesat(1, .4, .4, np.array([0, 0, 0, 0]))
+        CubeSat = sim.sat
         sim.inertial_to_body_quat = eul_to_quat(np.deg2rad(np.array([0,0,10])))
-        sim.sat.visualise()
+        # CubeSat.visualise()
         # sim.show_attitude()
-        sim.visualise_3d(True)
-        sim.sat.calculate_inertia()
+        # sim.visualise_3d(True)
+        CubeSat.panel_angles = np.array([20, 20, 20, 20])
+
+        # CubeSat.calculate_inertia()
+
         # sim.visualise_3d(True,test_array)
         # particles = sim.sat.generate_impacting_particle(n_particles=600)
         # print(particles[1])
@@ -42,5 +46,5 @@ if __name__ == "__main__":
     # animate_rotations(state)
 
 
-    #TODO: Implement some function to calculate/approximate panel moments of inertia for later dynamics implementation
+    #TODO: It might be interesting to build a control system which only uses the panel angles
     #TODO: Straight-on particles with zero degree panels results in Polygon error. Investigate!
